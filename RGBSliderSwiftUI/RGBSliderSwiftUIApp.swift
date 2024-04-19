4cc7412cd1c190bbb63553eaf73304a1aeb07ab2
Tatiana Lazarenko <tan.strebkova@gmail.com>
2024-04-19 14:01:40 +0300

    Initial Commit

---
+++ RGBSliderSwiftUI/RGBSliderSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  RGBSliderSwiftUIApp.swift
+//  RGBSliderSwiftUI
+//
+//  Created by Tatiana Lazarenko on 4/19/24.
+//
+
+import SwiftUI
+
+@main
+struct RGBSliderSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
