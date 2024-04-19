c9f596591bc778ad87b8c886eb18c66f974b4321
Tatiana Lazarenko <tan.strebkova@gmail.com>
2024-04-19 18:28:31 +0300

    add textFields, add keybord

--- RGBSliderSwiftUI/ColorSliderView.swift
+++ RGBSliderSwiftUI/ColorSliderView.swift
@@ -9,18 +9,43 @@ import SwiftUI
 
 struct ColorSliderView: View {
     let color: Color
+    
     @Binding var sliderValue: Double
+    @Binding var inputValue: String
+    
+    @FocusState var isFocused: Bool
     
     var body: some View {
         HStack {
             Text(lround(sliderValue).formatted()).foregroundStyle(.white)
                 .frame(width: 40)
+            
             Slider(value: $sliderValue, in: 0...255, step: 1)
                 .tint(color)
+            
+            TextField(lround(sliderValue).formatted(), text: $inputValue)
+                .frame(width: 60)
+                .textFieldStyle(.roundedBorder)
+                .multilineTextAlignment(.trailing)
+                .onChange(of: inputValue) { _, newValue in
+                    if let value = Double(newValue) {
+                        sliderValue = value
+                    }
+                }
+                .keyboardType(.numberPad)
+                .focused($isFocused)
+                .toolbar {
+                    ToolbarItemGroup(placement: .keyboard) {
+                        Spacer()
+                        Button("Done") {
+                            isFocused = false
+                        }
+                    }
+                }
         }
     }
 }
 
 #Preview {
-    ColorSliderView(color: .red, sliderValue: .constant(100.0))
+    ColorSliderView(color: .red, sliderValue: .constant(100.0), inputValue: .constant("100"))
 }
--- RGBSliderSwiftUI/ContentView.swift
+++ RGBSliderSwiftUI/ContentView.swift
@@ -8,23 +8,39 @@
 import SwiftUI
 
 struct ContentView: View {
-    @State var redSliderValue = 200.0
-    @State var greenSliderValue = 200.0
-    @State var blueSliderValue = 200.0
+    @State var redSliderValue = Double.random(in: 0...255)
+    @State var greenSliderValue = Double.random(in: 0...255)
+    @State var blueSliderValue = Double.random(in: 0...255)
+    
+    @State var redInputValue = ""
+    @State var greenInputValue = ""
+    @State var blueInputValue = ""
     
     var body: some View {
         VStack(spacing: 20) {
             ColorView(color: Color(red: redSliderValue/255, green: greenSliderValue/255, blue: blueSliderValue/255))
                 .padding(.bottom, 40)
             
-            ColorSliderView(color: .red, sliderValue: $redSliderValue)
-            ColorSliderView(color: .green, sliderValue: $greenSliderValue)
-            ColorSliderView(color: .blue, sliderValue: $blueSliderValue)
+            ColorSliderView(color: .red, sliderValue: $redSliderValue, inputValue: $redInputValue)
+            ColorSliderView(color: .green, sliderValue: $greenSliderValue, inputValue: $greenInputValue)
+            ColorSliderView(color: .blue, sliderValue: $blueSliderValue, inputValue: $blueInputValue)
             
             Spacer()
+            
         }
         .padding()
         .background(Color(red: 1/255, green: 107/255, blue: 193/255))
+        .onTapGesture {
+            hideKeyboard()
+        }
+    }
+    
+}
+
+extension View {
+    func hideKeyboard() {
+        let resign = #selector(UIResponder.resignFirstResponder)
+        UIApplication.shared.sendAction(resign, to: nil, from: nil, for: nil)
     }
 }
 
